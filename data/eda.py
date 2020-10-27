786f440f72617127036d8ee9eb0f354db48a15a0
MarcosMorales2011 <40769305+LiGhTisTiGhT@users.noreply.github.com>
2020-10-27 08:56:16 -0600

    Please work.

--- data/eda.py
+++ data/eda.py
@@ -4,16 +4,43 @@
 import pandas as pd
 import numpy as np
 
-list_2 = [12, 52, 2234, 334/2]
+
+class Deque():
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def addRear(self, item):
+        self.items.insert(0, item)
+
+    def addFront(self, item):
+        self.items.append(item)
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def removeFront(self):
+        return self.items.pop(-1)
+
+    def size(self):
+        return len(self.items)
+
+
 def date_three_col(dr):
-  r = pd.to_datetime(dr)
-  a = pd.DatetimeIndex(dr).year
-  b = pd.DatetimeIndex(dr).month
-  c = pd.DatetimeIndex(dr).day
-  return a, b, c
+    r = pd.to_datetime(dr)
+    a = pd.DatetimeIndex(dr).year
+    b = pd.DatetimeIndex(dr).month
+    c = pd.DatetimeIndex(dr).day
+    return a, b, c
+
 
 def all_nulls(dr):
-  return dr.isnull().sum().sum()
+    return dr.isnull().sum().sum()
+
 
 def outliers(X):
-  return X[X.between(X.quantile(0.01), X.quantile(0.99))]
+    return X[
+      X.between(X.quantile(0.01), X.quantile(0.99))]
--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 
-    name="lambdatamarcosmorales",  # Replace with your own username
-    version="0.1.5",
+    name="DataScienceFuncs",  # Replace with your own username
+    version="0.0.1",
     author="Marcos Morales",
     author_email="marcosmorales2011@gmail.com",
     description="A small package for Data Science functions.",
